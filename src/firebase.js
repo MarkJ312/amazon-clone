774326b01da2e82dfe24313867d6a0ecd8f3e64b
MarkJ312 <65988322+MarkJ312@users.noreply.github.com>
2020-09-09 13:31:31 -0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9d9fLv7WJJQhuTRaFWwOeQvqOYoDsU8w",
+    authDomain: "clone-d6437.firebaseapp.com",
+    databaseURL: "https://clone-d6437.firebaseio.com",
+    projectId: "clone-d6437",
+    storageBucket: "clone-d6437.appspot.com",
+    messagingSenderId: "121850895372",
+    appId: "1:121850895372:web:50314634463407557d2229",
+    measurementId: "G-93JR2M2DCY"
+  };
